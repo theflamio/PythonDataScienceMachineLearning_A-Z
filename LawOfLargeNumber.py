dd258d08bb0f25c31b20c7b8d5db56464e3c6b89
Flemming <flemming.christensen.fc@siemensgamesa.com>
2021-09-07 15:36:20 +0200

    Homework LLN

---
+++ LawOfLargeNumber.py
@@ -0,0 +1,44 @@
+''' 
+    
+    Law of large numbers 
+
+    Xn -> E(x) when n -> infinity
+
+    Xn er gennemsnit af eksemplevis højden på n personer
+    
+'''
+
+'''
+    
+    Eksemple med brug af Law of Large numbers
+    
+    10 mønt kast forventer man 50/50 plat og krone
+    
+    10: kast resultat 7/3 = 70% / 30% hvilket afviger fra forvented
+    
+    100: kast blev resultat 52/48 52% / 48% tættere på det forventede
+    
+    1000: kast 502 / 498 -> 50.2% / 49.8%
+    
+    when the sample sice increase we get closer to the expected result
+    
+'''
+
+'''
+
+    Home work test the Law Of Large Numbers for N random normally distribute numbers with 
+    mean = 0:
+    stdev = 1:
+        
+    Create a python script tat will count how many of these numbers fall between -1 and 1 
+    and divide by the total quantity of N
+    
+    You know that E(x) = 68.2%
+    
+    Check that mean(Xn) -> E(x) as you rerun your script while increasing N
+
+'''
+
+
+
+normalvarie()
