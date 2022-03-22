217cdb5287e88eebb1c800eae27378e2878a8d71
Flemming Christensen <flamekfor@gmail.com>
2022-03-22 22:19:18 +0100

    Playing with new extensions

---
+++ Matrices.py
@@ -0,0 +1,11 @@
+# FIXME
+# REVIEW
+# TODO
+# NOTE
+
+def monkey(monkeyName, age):
+    print(f"Monkey says oh oh oh {monkeyName} you are {age} years old")
+    return monkey
+
+d = monkey("Tom", 32)
+print(d)
